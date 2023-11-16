c1ea8b70f6ed650199c35191a4b3e75b48c46fc6
FC-Sebastian <sebastian.briese@fatchip.de>
2023-11-16 17:26:54 +0100

    y

--- extend/Application/Model/Payment.php
+++ extend/Application/Model/Payment.php
@@ -5,7 +5,7 @@ namespace Fatchip\FATPay\extend\Application\Model;
 use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
 use OxidEsales\EshopCommunity\Internal\Framework\Database\QueryBuilderFactoryInterface;
 
-class Payment extends Payment_parent
+class Payment extends Payment_Parent
 {
     public function fcHasFatPay()
     {
