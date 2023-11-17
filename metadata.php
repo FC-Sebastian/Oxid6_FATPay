2945de34b285e0959df19a9425fb074c58f888a0
FC-Sebastian <sebastian.briese@fatchip.de>
2023-11-17 17:19:21 +0100

    bro wtffffffff

--- metadata.php
+++ metadata.php
@@ -24,7 +24,7 @@ $aModule = [
         [
             'template' => 'page/checkout/inc/payment_other.tpl',
             'block'    => 'checkout_payment_longdesc',
-            'file'     => 'checkout_payment_longdesc.tpl'
+            'file'     => 'out/blocks/checkout_payment_longdesc.tpl'
         ],
     ],
     'events'      => [
