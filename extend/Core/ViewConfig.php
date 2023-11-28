82c43e726968b8237cc3256b34043a30f3a1717e
FC-Sebastian <sebastian.briese@fatchip.de>
2023-11-28 11:53:58 +0100

    smoother a brain cant be, so smooth so me

--- extend/Core/ViewConfig.php
+++ extend/Core/ViewConfig.php
@@ -7,7 +7,7 @@ class ViewConfig extends ViewConfig_parent
     public function fatpayGetModuleVersion()
     {
         $oContainer = \OxidEsales\EshopCommunity\Internal\Container\ContainerFactory::getInstance()->getContainer();
-        $oContainer = $oContainer->get(\OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Bridge\ShopConfigurationDaoBridgeInterface::class);
+        $oContainer = $oContainer->get(\OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Bridge\ShopConfigurationDaoBridgeInterface::class)->get();
         return json_encode(get_class_methods($oContainer));
     }
 }
